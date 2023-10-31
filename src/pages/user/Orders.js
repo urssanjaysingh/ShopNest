5da5f0ead1bc70c8e6933a40fc23986de4816fa2
urssanjaysingh <sanjaysingh26112000@gmail.com>
2023-10-31 06:27:00 +0530

    Changes

--- src/pages/user/Orders.js
+++ src/pages/user/Orders.js
@@ -31,7 +31,7 @@ const Orders = () => {
                     <div className="col-md-3">
                         <UserMenu />
                     </div>
-                    <div className="col-md-9 d-flex justify-content-center">
+                    <div className="col-md-9">
                         <h1 className='text-center'>All Orders</h1>
                         {
                             orders?.map((o, i) => {
