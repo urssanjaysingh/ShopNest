2630f433df1bd078d17816f6e3ed659de2a6a00d
urssanjaysingh <sanjaysingh26112000@gmail.com>
2023-10-31 16:31:49 +0530

    Changes

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
+import { Spin } from 'antd';
 
 const Spinner = ({ path = 'login' }) => {
     const [count, setCount] = useState(5)
@@ -7,25 +8,29 @@ const Spinner = ({ path = 'login' }) => {
     const location = useLocation()
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCount((prevValue) => --prevValue)
-        }, 1000)
-        count === 0 && navigate(`/${path}`, {
-            state: location.pathname,
-        });
-        return () => clearInterval(interval)
-    }, [count, navigate, location, path])
+        // Implement your countdown logic here and update the 'count' state
+        const countdownTimer = setInterval(() => {
+            setCount((prevCount) => prevCount - 1);
+        }, 1000);
+
+        // When count reaches 0, navigate to the specified path
+        if (count === 0) {
+            navigate(`/${path}`, {
+                state: location.pathname,
+            });
+        }
+
+        return () => {
+            clearInterval(countdownTimer);
+        };
+    }, [count, navigate, path, location]);
 
     return (
-        <>
-            <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "100vh" }}>
-                <h1 className="text-center">redirecting to you in {count} second</h1>
-                <div className="spinner-border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-            </div>
-        </>
-    )
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "100vh" }}>
+            <h1 className="text-center">Redirecting to you in {count} second{count !== 1 ? 's' : ''}</h1>
+            {count !== 0 ? <Spin size="large" /> : null}
+        </div>
+    );
 }
 
 export default Spinner
